b3699b4872c736d1df46e89afd7ebfecb17cd01e
Praja04 <rajadamang13@gmail.com>
2025-09-01 08:58:54 +0700

    test 9 rev 5

--- controllers/retail_d5_controller.go
+++ controllers/retail_d5_controller.go
@@ -62,51 +62,32 @@ func getCurrentShift(t time.Time) int {
 		return 3
 	}
 }
-func min(a, b time.Time) time.Time {
-	if a.Before(b) {
-		return a
-	}
-	return b
-}
+
 func UptimeStartMesinRealtime(c *gin.Context) {
-	loc, _ := time.LoadLocation("Asia/Jakarta")
 	dateParam := c.Query("date")
 	var date time.Time
 	var err error
 
 	if dateParam == "" {
-	date = time.Now().In(loc)
+		date = time.Now()
 	} else {
 		date, err = time.Parse("2006-01-02", dateParam)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Format tanggal salah. Gunakan YYYY-MM-DD"})
 			return
 		}
-		// Normalisasi ke zona Jakarta
-		date = time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, loc)
 	}
 
-	now := time.Now().In(loc)
-
 	var shifts []gin.H
-	
+	now := time.Now()
 
 	for i := 1; i <= 3; i++ {
 		start, end := getShiftRange(date, i)
-		// startUTC := start.In(time.UTC)
-		// endUTC := end.In(time.UTC)
-
 
 		var countSeconds int64
-		if now.Before(start) {
-			countSeconds = 0
-		} else {
-			config.DB.Model(&models.RetailD5{}).
-				Where("start_mesin = ? AND DATE(ts) = ?", 1, date.Format("2006-01-02")).
-				Where("ts >= ? AND ts <= ?", start.In(time.UTC), min(end.In(time.UTC), now.In(time.UTC))).
-				Count(&countSeconds)
-		}
-
+		config.DB.Model(&models.RetailD5{}).
+			Where("start_mesin = ? AND ts >= ? AND ts <= ?", 1, start, end).
+			Count(&countSeconds)
 
 		// Konversi runtime ke menit
 		runtimeMinutes := countSeconds / 60
@@ -114,12 +95,12 @@ func UptimeStartMesinRealtime(c *gin.Context) {
 		// Hitung actual shift time
 		var actualMinutes int64
 		if now.Before(start) {
-		actualMinutes = 0
-		} else if now.After(end) {
-			actualMinutes = int64(end.Sub(start).Minutes())
-		} else {
-			actualMinutes = int64(now.Sub(start).Minutes())
-		}
+	actualMinutes = 0
+} else if now.After(end) {
+	actualMinutes = int64(end.Sub(start).Minutes()) // full shift duration
+} else {
+	actualMinutes = int64(now.Sub(start).Minutes()) // partial shift
+}
 
 		// Hitung uptime
 		uptime := 0.0
